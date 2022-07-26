af5c5787a23ba2a53515566ea1c103d9f2ef4e84
Roshan nahak <roshannahak1122@gmail.com>
2022-07-27 00:53:53 +0530

    add custom app bar

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart' hide Colors;
+import 'package:zinotalens/pages/cart_view_page.dart';
+
+import '../utils/colors.dart';
+
+AppBar customAppBar(BuildContext context,
+        {String title = "ZinotaLens",
+        bool isSearchIcon = false,
+        bool isCartIcon = true}) =>
+    AppBar(
+      backgroundColor: Colors.skyBlue,
+      shadowColor: Colors.transparent,
+      iconTheme: IconThemeData(color: Colors.white),
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.white),
+      ),
+      actions: [
+        isSearchIcon
+            ? IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.search),
+                splashRadius: 20,
+              )
+            : SizedBox(),
+        isCartIcon
+            ? IconButton(
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CartViewPage())),
+                icon: Icon(Icons.shopping_cart),
+                splashRadius: 20,
+              )
+            : SizedBox(),
+      ],
+    );
