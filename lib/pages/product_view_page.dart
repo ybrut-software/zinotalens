b2ec6d8762d99ac8d40d0f1c2279ecaec289c281
Roshan nahak <roshannahak1122@gmail.com>
2022-09-23 19:11:31 +0530

    handle network error in product view page

--- lib/pages/product_view_page.dart
+++ lib/pages/product_view_page.dart
@@ -40,72 +40,78 @@ class _ProductViewPageState extends State<ProductViewPage> {
       appBar: customAppBar(context, title: ""),
       body: productProvider.isDataLoad
           ? circularProgressIndicator()
-          : Stack(
-              children: [
-                ListView(padding: EdgeInsets.only(bottom: 70), children: [
-                  productViewPageWidget.ProductImageSliderAndDesc(
-                      photo: fetchImageController(photoUrl: product.photos![0]),
-                      title: product.title!,
-                      frameSize: product.specifications?.size,
-                      frameWidth: product.specifications?.width,
-                      rating: product.averageRating?.toDouble(),
-                      stockQuantity: product.stockQuantity,
-                      sellingPrice: product.salesPrice!,
-                      listingPrice: product.listPrice),
-                  SizedBox(height: 5),
-                  productViewPageWidget.DeliveryOptionsView(
-                      warrantyTime: product.specifications?.warranty),
-                  SizedBox(height: 5),
-                  productViewPageWidget.AboutProduct(
-                      description: product.description,
-                      frameStyle: product.specifications?.frameStyle1,
-                      productId: product.productId!),
-                  SizedBox(height: 5),
-                  productViewPageWidget.ProductSpecification(
-                      specifications: product.specifications!)
-                ]),
-                Positioned(
-                  bottom: 0.0,
-                  left: 0.0,
-                  right: 0.0,
-                  child: ElevatedButton(
-                      onPressed: () {
-                        ProductAddCartModel productCart = ProductAddCartModel(
-                            productId: product.productId!,
-                            productTitle: product.title!,
-                            productPhoto: product.photos![0],
-                            productSize: product.specifications!.size!,
-                            productListingPrice: product.listPrice,
-                            productSellingPrice: product.salesPrice!,
-                            productQuantity: 1);
-                        if (cart.isProductInCart(productObj: productCart))
-                          cart.addCartItemProvider(productCart: productCart);
-                        else {
-                          showSnackBarMessage(
-                              context, "Item is already in cart!");
-                        }
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.skyBlue),
-                        shadowColor:
-                            MaterialStateProperty.all(Colors.transparent),
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(5))),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.all(18),
-                        child: Text(
-                          "ADD TO CART",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500),
-                        ),
-                      )),
+          : productProvider.isError
+              ? errorWidget(errorMsg: productProvider.errorMsg)
+              : Stack(
+                  children: [
+                    ListView(padding: EdgeInsets.only(bottom: 70), children: [
+                      productViewPageWidget.ProductImageSliderAndDesc(
+                          photo: fetchImageController(
+                              photoUrl: product.photos![0]),
+                          title: product.title!,
+                          frameSize: product.specifications?.size,
+                          frameWidth: product.specifications?.width,
+                          rating: product.averageRating?.toDouble(),
+                          stockQuantity: product.stockQuantity,
+                          sellingPrice: product.salesPrice!,
+                          listingPrice: product.listPrice),
+                      SizedBox(height: 5),
+                      productViewPageWidget.DeliveryOptionsView(
+                          warrantyTime: product.specifications?.warranty),
+                      SizedBox(height: 5),
+                      productViewPageWidget.AboutProduct(
+                          description: product.description,
+                          frameStyle: product.specifications?.frameStyle1,
+                          productId: product.productId!),
+                      SizedBox(height: 5),
+                      productViewPageWidget.ProductSpecification(
+                          specifications: product.specifications!)
+                    ]),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            ProductAddCartModel productCart =
+                                ProductAddCartModel(
+                                    productId: product.productId!,
+                                    productTitle: product.title!,
+                                    productPhoto: product.photos![0],
+                                    productSize: product.specifications!.size!,
+                                    productListingPrice: product.listPrice,
+                                    productSellingPrice: product.salesPrice!,
+                                    productQuantity: 1);
+                            if (cart.isProductInCart(productObj: productCart))
+                              cart.addCartItemProvider(
+                                  productCart: productCart);
+                            else {
+                              showSnackBarMessage(
+                                  context, "Item is already in cart!");
+                            }
+                          },
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.skyBlue),
+                            shadowColor:
+                                MaterialStateProperty.all(Colors.transparent),
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5))),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.all(18),
+                            child: Text(
+                              "ADD TO CART",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          )),
+                    ),
+                  ],
                 ),
-              ],
-            ),
     );
   }
 }
--- lib/provider/product_details_provider.dart
+++ lib/provider/product_details_provider.dart
@@ -1,10 +1,17 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:zinotalens/controller/products_controller.dart';
 import 'package:zinotalens/model/product_details_model.dart';
+import 'package:zinotalens/utils/exception_handler.dart';
 
 class ProductDetailsProvider extends ChangeNotifier {
   Product _product = Product();
   bool _isDataLoad = true;
+
+  bool _isError = false;
+  bool get isError => _isError;
+  String _errorMsg = "";
+  String get errorMsg => _errorMsg;
 
   Product get getProduct => _product;
   bool get isDataLoad => _isDataLoad;
@@ -13,9 +20,14 @@ class ProductDetailsProvider extends ChangeNotifier {
     try {
       _product = await fetchProductDetails(productId);
       _isDataLoad = false;
+      _isError = false;
     } catch (e) {
       print("error no 823: $e");
       _isDataLoad = false;
+      _isError = true;
+      DioError dioError = e as DioError;
+      String error = DioExceptions.fromDioError(dioError).toString();
+      _errorMsg = error;
     }
     notifyListeners();
   }
